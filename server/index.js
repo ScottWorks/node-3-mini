fe26471e4c48e1dbe07f9be238eed1cff9fb0f7f
ScottWorks <scottworks.io@gmail.com>
2018-04-10 12:43:07 -0600

    completed assignment

--- server/index.js
+++ server/index.js
@@ -1,16 +1,43 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mc = require( `./controllers/messages_controller` );
+const createInitialSession = require('./middleware/session');
+const filter = require('./middleware/filter');
+const mc = require(`./controllers/messages_controller`);
+const session = require('express-session');
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../build` ) );
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../build`));
 
-app.post( "/api/messages", mc.create );
-app.get( "/api/messages", mc.read );
-app.put( "/api/messages", mc.update );
-app.delete( "/api/messages", mc.delete );
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 10000
+    }
+  })
+);
 
-const port = process.env.PORT || 3000
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+app.use((req, res, next) => createInitialSession(req, res, next));
+app.use((req, res, next) => {
+  if (req.method === 'POST' || req.method === 'PUT') {
+    filter(req, res, next);
+  } else {
+    next();
+  }
+});
+
+app.post('/api/messages', mc.create);
+app.get('/api/messages', mc.read);
+app.get('/api/messages/history', mc.delete);
+app.put('/api/messages', mc.update);
+app.delete('/api/messages', mc.delete);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
